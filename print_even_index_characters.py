87876c05705be6d4e06874c92cbc361a6033e2c1
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-21 00:44:48 +0800

    pseudocode

--- print_even_index_characters.py
+++ print_even_index_characters.py
@@ -1,3 +1,18 @@
 # Exercise 3: Print characters from a string that are present at an even index number
 # Write a program to accept a string from the user and display characters that are present at an even index number.
 # For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’. 
+
+# Orginal String is  pynative
+# Printing only even index chars
+# p
+# n
+# t
+# v
+
+#Pseudocode 
+
+# Let the user input string 
+# Create a len function to calculate the length of the inputted string 
+# Print the first message
+# Print the second message 
+# Create a loop 
